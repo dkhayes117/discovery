96112730ee9688ac2d96a23f3c4995c2dde7f790
dkhayes117 <dkhayes117@yahoo.com>
2021-06-18 21:13:52 -0400

    Forked the Discovery Book and push led-roulette changes

--- src/05-led-roulette/src/main.rs
+++ src/05-led-roulette/src/main.rs
@@ -2,14 +2,23 @@
 #![no_main]
 #![no_std]
 
-use aux5::entry;
+use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let led_period = 50_u16;
+
+    loop {
+        for i in 0..=7 {
+            let next = (i + 1) % 8;
+            leds[next].on().ok();
+            delay.delay_ms(led_period);
+
+            leds[i].off().ok();
+            delay.delay_ms(led_period);
+        }
+
+    }
 }
