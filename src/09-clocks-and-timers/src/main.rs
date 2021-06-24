298c64edfb178da530a1631be42912e23f3fb9a4
dkhayes117 <dkhayes117@yahoo.com>
2021-06-24 19:33:47 -0400

    Chapter 9: Timer for LED Roulette

--- src/09-clocks-and-timers/src/main.rs
+++ src/09-clocks-and-timers/src/main.rs
@@ -5,7 +5,14 @@ use aux9::{entry, switch_hal::OutputSwitch, tim6};
 
 #[inline(never)]
 fn delay(tim6: &tim6::RegisterBlock, ms: u16) {
-    // TODO implement this
+    // Set the autoreload register to ms value
+    tim6.arr.write(|w| w.arr().bits(ms));
+    // enable the counter
+    tim6.cr1.modify(|_,w| w.cen().set_bit());
+    // Busy waiting for UIF bit to be set
+    while !tim6.sr.read().uif().bit_is_set() {}
+    // Clear the update event flag
+    tim6.sr.modify(|_,w| w.uif().clear_bit());
 }
 
 #[entry]
@@ -13,7 +20,14 @@ fn main() -> ! {
     let (leds, rcc, tim6) = aux9::init();
     let mut leds = leds.into_array();
 
-    // TODO initialize TIM6
+    // Add power to the TIM6 timer
+    rcc.apb1enr.modify(|_,w|w.tim6en().set_bit());
+
+    // One-pulse mode, keep counter disabled
+    tim6.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
+
+    // Configure the timer Prescaler value for 1 Khz
+    tim6.psc.write(|w| w.psc().bits(7999));
 
     let ms = 50;
     loop {
