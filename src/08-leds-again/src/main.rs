d4891ed2297844f55f0d60e53aa925b4e1b53efd
dkhayes117 <dkhayes117@yahoo.com>
2021-06-24 19:01:15 -0400

    Up to Chapter 8

--- src/08-leds-again/src/main.rs
+++ src/08-leds-again/src/main.rs
@@ -8,7 +8,20 @@ use aux8::entry;
 fn main() -> ! {
     let (gpioe, rcc) = aux8::init();
 
-    // TODO initialize GPIOE
+    // Power GPIO-E
+    rcc.ahbenr.modify(|_,w| w.iopeen().set_bit());
+
+    // Enable Digital Output on GPIO-E
+    gpioe.moder.modify(|_,w|{
+        w.moder8().output();
+        w.moder9().output();
+        w.moder10().output();
+        w.moder11().output();
+        w.moder12().output();
+        w.moder13().output();
+        w.moder14().output();
+        w.moder15().output()
+    });
 
     // Turn on all the LEDs in the compass
     gpioe.odr.write(|w| {
